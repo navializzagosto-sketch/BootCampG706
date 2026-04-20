19302cf72940204d3a6c3b67c6c26e3ca9f2f80d
Elizabeth Navia Mejia <navializz.agosto@gmail.com>
2026-04-20 11:35:48 -0500

    trabajo en clase pagina web elizabeth

---
+++ mision2/chatv1/chatv1.py
@@ -0,0 +1,4 @@
+from sklearn.feature_extraction.text import CountVectorizer
+# CountVectorizer convierte texto en un vector
+from sklearn.naive_bayes import MultinomialNB
+# MultinomialNB modelo de inteligencia artificial que aprende relaciones entre texto y respuesta 
---
+++ mision2/implementacion.md
@@ -0,0 +1,9 @@
+# guia de implementacion del chatbot (supervisado)
+python -m pip install nltk
+ejemplo nltk
+import nltk
+nltk.download('punkt')
+
+instala librerias
+pip install scikit-learn nltk numpy gym==0.26.2 gym-notices" 
+´
